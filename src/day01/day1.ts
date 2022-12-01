c6ec0565bf267c87233509b6a65871b68b24100d
Jarrod Zywien <jarrod.zywien@moodys.com>
2022-12-01 16:04:53 -0500

    use shorthand for array sum

--- src/day01/day1.ts
+++ src/day01/day1.ts
@@ -1,11 +1,12 @@
+import "../util/array";
+
 export const day1step1 = (data: number[][]): number =>
-  Math.max(...data.map((values) => values.reduce((s, c) => s + c, 0)));
+  Math.max(...data.map((values) => values.sum()));
 
 export const day1step2 = (data: number[][], topN: number = 3): number => {
   return data
-    .reduce((top3, curr) => {
-      const sum = curr.reduce((s, c) => s + c, 0);
-      return [...top3, sum].sort((a, b) => b - a).slice(0, topN);
-    }, [])
-    .reduce((sum, curr) => sum + curr, 0);
+    .map((values) => values.sum())
+    .sort((a, b) => b - a)
+    .slice(0, 3)
+    .sum();
 };
