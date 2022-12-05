942adb3460488e5082b4f394f68acf83feb557f3
Jarrod Zywien <jarrod.zywien@moodys.com>
2022-12-05 08:26:13 -0500

    day 5 solutions

--- src/day05/day5.test.ts
+++ src/day05/day5.test.ts
@@ -2,26 +2,28 @@ import { step1, step2 } from './day5';
 import sample from './sample.json';
 import input from './input.json';
 
+const deepCopy = (input: any) => JSON.parse(JSON.stringify(input));
+
 describe('day 5', () => {
    describe('step 1', () => {
       test('sample', () => {
-         const result = step1(sample);
-         expect(result).toEqual(-1);
+         const result = step1(deepCopy(sample));
+         expect(result).toEqual('CMZ');
       });
       test('input', () => {
-         const result = step1(input);
-         expect(result).toEqual(-1);
+         const result = step1(deepCopy(input));
+         expect(result).toEqual('FJSRQCFTN');
       });
    });
 
    describe('step 2', () => {
       test('sample', () => {
-         const result = step2(sample);
-         expect(result).toEqual(-1);
+         const result = step2(deepCopy(sample));
+         expect(result).toEqual('MCD');
       });
       test('input', () => {
-         const result = step2(input);
-         expect(result).toEqual(-1);
+         const result = step2(deepCopy(input));
+         expect(result).toEqual('CJVLJQPHS');
       });
    });
 });
--- src/day05/day5.ts
+++ src/day05/day5.ts
@@ -1,3 +1,28 @@
-export const step1 = (data: any): number => 0;
+export type Stack = string[];
+export type Move = number[];
+export type Game = {
+   stacks: Stack[];
+   moves: Move[];
+};
 
-export const step2 = (data: any): number => 0;
+const moveStacks = (move: Move, stacks: Stack[], strategy: (stack: Stack) => Stack) => {
+   const [num, from, to] = move;
+   const moved = strategy(stacks[from - 1].splice(-num));
+   stacks[to - 1].push(...moved);
+   return stacks;
+};
+
+const noOpStrategy = (stack: Stack) => stack;
+const reverseStrategy = (stack: Stack) => {
+   return stack.reverse();
+};
+
+export const step1 = (game: Game): string =>
+   game.moves
+      .reduce((stacks, move) => moveStacks(move, stacks, reverseStrategy), [...game.stacks])
+      .reduce((top, stack) => `${top}${stack.pop()}`, '');
+
+export const step2 = (game: Game): string =>
+   game.moves
+      .reduce((stacks, move) => moveStacks(move, stacks, noOpStrategy), [...game.stacks])
+      .reduce((top, stack) => `${top}${stack.pop()}`, '');
