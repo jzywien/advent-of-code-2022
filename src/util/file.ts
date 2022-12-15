c5645bfd168bd414afd468ecfc354482ed009210
Jarrod Zywien <jarrod.zywien@moodys.com>
2022-12-15 14:25:58 -0500

    remove redundant await

--- src/util/file.ts
+++ src/util/file.ts
@@ -2,6 +2,6 @@ import { readFile } from 'node:fs/promises';
 
 export const getInput = async (dir: string, file: string, ext: string = '.txt'): Promise<string> => {
    const input = await readFile(`${dir}/${file}${ext}`);
-   const text = await input.toString();
+   const text = input.toString();
    return text;
 };
