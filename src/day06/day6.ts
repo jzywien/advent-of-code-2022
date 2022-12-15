fb64b711d6c384507613eb685aa7089a800e7488
Jarrod Zywien <jarrod.zywien@moodys.com>
2022-12-15 17:08:15 -0500

    update var name from data to input for consistency

--- src/day06/day6.ts
+++ src/day06/day6.ts
@@ -1,2 +1,2 @@
-export const solution = (data: string, matchLen: number = 4): number =>
-   data.split('').findIndex((_, ndx, orig) => new Set(orig.slice(ndx, ndx + matchLen)).size === matchLen) + matchLen;
+export const solution = (input: string, matchLen: number = 4): number =>
+   input.split('').findIndex((_, ndx, orig) => new Set(orig.slice(ndx, ndx + matchLen)).size === matchLen) + matchLen;
